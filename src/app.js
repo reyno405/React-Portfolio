75eb303a27dda81d19ebc25fc2bbce3bb5271382
kevin reynolds <t6500kr@outlook.com>
2022-12-05 11:24:36 -0500

    this update has a index html, app.js and footer.css

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
-import './app.css';
+import React from "react";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import "./app.css";
 
 function App() {
-    return(
-        <div className='App'>
-            <Header/>
-            <Footer/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
