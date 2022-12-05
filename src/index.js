0142ac42ab55f99578c912ee0076158729ecdbaf
kevin reynolds <t6500kr@outlook.com>
2022-12-05 10:41:15 -0500

    this update has the code for the index.js

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { HashRouter as Router} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import ParticlesBg from "particles-bg";
+
+ReactDOM.render(
+    <Router>
+        <App />
+        <ParticlesBg type="square" bg={true}/>
+    </Router>,
+    document.getElementById('root')
+);
+
+serviceWorker.unregister();
